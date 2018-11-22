4c3fd4215c9ebcbabc811c770fe327f72013bd7f
Miniroups <42999204+Miniroups@users.noreply.github.com>
2018-11-22 11:12:24 +0100

    Ajout du fichier avec les cartes hackers à copier

---
+++ cardsAdded.js
@@ -0,0 +1,20 @@
+    hackerCard : {
+      description : "Je te DDOS quand tu stream une partie d'Overwatch.",
+      power : 3,
+    },
+    hackerCard : {
+      description : "Je te hack ton compte Skype pour draguer tes contacts.",
+      power : 2,
+    },
+    hackerCard : {
+      description : "Je vols des cordonnées bancaires sur des sites non-sécurisés.",
+      power : 4,
+    },
+    hackerCard : {
+      description : "Je vols des mots de passe faible via un logiciel de force brute.",
+      power : 1,
+    },
+    hackerCard : {
+      description : "Je laisse des clefs USB pleine de virus dans les Starbucks du coin.",
+      power : 5,
+    },
